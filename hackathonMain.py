043f903384f140e2dc54e4affc3890372486566d
Leif Keane <kdopo@udel.edu>
2024-03-02 13:05:45 -0500

    added basics + toTime

--- hackathonMain.py
+++ hackathonMain.py
@@ -1,4 +1,34 @@
 from dataclasses import dataclass
 from matplotlib import *
 from bakery import assert_equal
-# imports
+# imports for functionality
+'''
+CHECKLIST:
+    BASICS:
+            Imports [X]
+            ? []
+            ? []
+    DATA:
+        Formatting []
+        
+'''
+'''
+    funcName:
+    
+    Arg(s):
+        blah (type):
+    
+    Returns:
+        type:
+    
+'''
+
+# constants vvv
+
+# dataclasses vvv
+
+#functions vvv
+
+def toTime (time : str,amPm : str) -> int:
+    int x = 0
+#end of function x
